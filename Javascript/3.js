381c967d9398a120c75768da4c6d03310c6dd45d
tiffanyposs <tiffanyposs@gmail.com>
2016-02-14 14:39:57 -0500

    updated solution to num 3

--- Javascript/3.js
+++ Javascript/3.js
@@ -1,45 +1,20 @@
-var number = 600851;
-var x = 1;
-var array = []
-
-//checks for whole  numbers when divided
-while(x < number && array.indexOf(x) === -1){
-	var y = number / x;
-	if(y % 1 === 0){
-		array.push(x, y);
-	} 
-	x++;
-}
-
-
-var prime_array = []
-
-//make an array of prime numbers
-array.forEach(function(each){
-	var num = 3;
-	var divide_array = [];
-	while(num < each){
-	var total = each / num;
-		if(total % 1 === 0){
-			divide_array.push(num)
+var isPrime = function(num){
+	for(var i = 2; i < num - 1; i++){
+		if(num % i == 0){
+			return false
 		}
-	num++;
 	}
-	if(divide_array.length === 0){
-		prime_array.push(each);
-	}
-})
-
-var high_prime = 1
-//finds the highest number
-for(i = 0, j = 1; j < prime_array.length; i++, j++){
-	if(prime_array[i] > prime_array[j] && prime_array[i] > high_prime){
-		high_prime = prime_array[i];
-	}
-	if(prime_array[j] > prime_array[i] && prime_array[j] > high_prime){
-		high_prime = prime_array[j];
-	}
+	return true
 }
 
-console.log(high_prime)
+var highestPrime = function(num){
+	var highest;
+	for(var i = 0; i <= num; i++){
+		if(num % i === 0 && isPrime(i)){
+			highest = i;
+		}
+	}
+	return highest
+}
 
+console.log(highestPrime(13195))
